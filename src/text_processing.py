be50eb0a58e4e22f2e376d33d532cc5fb0c1a4d1
Dilawar Singh <dilawars@ncbs.res.in>
2017-06-15 10:09:23 +0530

    Added function to write list/array to csv file. Suitable for small lists.

--- src/text_processing.py
+++ src/text_processing.py
@@ -31,3 +31,19 @@ def find_all_floats( text ):
     global float_regex_
     assert type( text ) == str
     return [ to_float( m.group(0) ) for m  in float_regex_.finditer( text ) ]
+
+def arrays2csv( outfile, **kwargs ):
+    """Write given vector/array to a CSV file. The separator is ',' by default.
+    It can not be changed. It can not be changed. It can not be changed.
+
+    The argument order will be preserved if python-3.6+ is used. Not
+    implementing the order preservation here.
+    """
+    header = kwargs.keys( )
+    sep = ','
+    with open( outfile, 'w' ) as f:
+        f.write( sep.join( header ) + '\n' )
+        cols = kwargs.values( )
+        for x in zip( *cols ):
+            x = map( str, x )
+            f.write( sep.join( x ) + '\n' )
