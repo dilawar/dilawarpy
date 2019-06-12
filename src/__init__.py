5c73dd8a8ceee00abd3f0afa042c535e601e69b5
Dilawar Singh <dilawar.s.rajput@gmail.com>
2019-06-12 14:32:35 +0530

    added hdf_store and hdf_load function to load store metadata.

--- src/__init__.py
+++ src/__init__.py
@@ -23,6 +23,7 @@ from .file_utils import *
 from .plot_utils import *
 from .statistics import *
 from .information_theory import *
+from .io_utils import *
 from .logger import *
 
 # random module.
