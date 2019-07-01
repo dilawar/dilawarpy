582dc14b98f368b804a751ea7c3967ba7288b814
Dilawar Singh <dilawar.s.rajput@gmail.com>
2019-07-01 17:06:11 +0530

    Bumped version.

--- dilawar/__init__.py
+++ dilawar/__init__.py
@@ -11,15 +11,15 @@ __email__            = "dilawars@ncbs.res.in"
 
 try:
     # This depends on scipy
-    from .data_analysis import *
-except ImportError as e:
+    from dilawar.data_analysis import *
+except ImportError:
     pass
 
-from .text_processing import *
-from .file_utils import *
-from .plot_utils import *
-from .statistics import *
-from .information_theory import *
-from .io_utils import *
-from .logger import *
-from .brian_utils import *
+from dilawar.text_processing import *
+from dilawar.file_utils import *
+from dilawar.plot_utils import *
+from dilawar.statistics import *
+from dilawar.information_theory import *
+from dilawar.io_utils import *
+from dilawar.logger import *
+from dilawar.brian_utils import *
