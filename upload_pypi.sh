bf5e612ceb9cf2d576e58551cd141c0ea56618f7
Dilawar Singh <dilawars@ncbs.res.in>
2019-08-14 17:32:25 +0530

    upload to pypi.

--- upload_pypi.sh
+++ upload_pypi.sh
@@ -2,6 +2,6 @@
 set -x
 set -e
 rm -rf dist
-nosetests dilawar || echo "Failed test"
-python setup.py sdist
-twine check dist/* && twine upload dist/*
+nosetests3 dilawar
+python3 setup.py sdist
+python3 -m twine check dist/* && twine upload dist/*
