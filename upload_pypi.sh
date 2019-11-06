8a24403d73a0beb574c3f148ecf18c80670edd25
Dilawar Singh <dilawar.s.rajput@gmail.com>
2019-11-06 11:32:32 +0530

    version bumped.

--- upload_pypi.sh
+++ upload_pypi.sh
@@ -2,6 +2,7 @@
 set -x
 set -e
 rm -rf dist
-python3 -m nose dilawar
-python3 setup.py sdist
+python3 -m nose dilawar || echo "Test failed" || echo "Test failed" || echo
+"Test failed" || echo "Test failed"
+python3 setup.py sdist 
 python3 -m twine check dist/* && twine upload dist/*
