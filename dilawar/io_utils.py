a7a08ee24985b729a390f9d4e21153b3f6b5a2f5
Dilawar Singh <dilawar.s.rajput@gmail.com>
2021-06-03 18:30:57 +0530

    All tests are passing.

--- dilawar/io_utils.py
+++ dilawar/io_utils.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 __author__ = "Dilawar Singh"
-__copyright__ = "Copyright 2019-, Dilawar Singh"
-__maintainer__ = "Dilawar Singh"
 __email__ = "dilawars@ncbs.res.in"
+
+from pathlib import Path
 
 # These are helper functions to store metadata to HDF5.
 # Snippets originally from https://stackoverflow.com/a/29130146/1805129
-def h5store(filename, df, key="mydata", **kwargs):
+def h5store(filename: Path, df, key: str = "mydata", **kwargs):
     import pandas as pd
 
     store = pd.HDFStore(filename, mode=kwargs.get("mode", "w"))
@@ -15,7 +15,7 @@ def h5store(filename, df, key="mydata", **kwargs):
     store.close()
 
 
-def h5load(filename, key="mydata"):
+def h5load(filename, key: str = "mydata"):
     import pandas as pd
 
     store = pd.HDFStore(filename)
@@ -28,24 +28,16 @@ save_df = h5store
 load_df = h5load
 
 
-def tests():
-    import os
+def test_io_utils():
     import numpy as np
     import pandas as pd
     import tempfile
 
-    storeName = os.path.join(tempfile.gettempdir(), "data.h5")
+    store = tempfile.NamedTemporaryFile(suffix='.h5')
     data = np.random.rand(100, 100)
     df = pd.DataFrame(data)
-    h5store(storeName, df, me="dilawar", mynum=301810)
-    s, m = h5load(storeName)
+    h5store(store.name, df, me="dilawar", mynum=301810)
+    s, m = h5load(store.name)
+    store.close()
     assert df.equals(s)
     assert m == dict(me="dilawar", mynum=301810)
-
-
-def main():
-    tests()
-
-
-if __name__ == "__main__":
-    main()
