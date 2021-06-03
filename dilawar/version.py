27a72dc53cf6e56b2a6298d888c72e31f9a28bd7
Dilawar Singh <dilawar.s.rajput@gmail.com>
2021-06-03 18:32:22 +0530

    removed version.

--- dilawar/version.py
+++
@@ -1,5 +0,0 @@
-import datetime
-
-now = datetime.datetime.now().strftime("%Y%m%d%H%M")
-
-__version__ = "0.8.%s" % now
--- upload_pypi.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -x
-set -e
-rm -rf dist
-python3 -m nose dilawar 
-python3 setup.py sdist 
-python3 -m twine check dist/* && twine upload dist/*
